e355d2f7accb35e6b49d27d6169d33171a105f28
Yu-Teng Li <yutengli@berkeley.edu>
2020-08-30 15:23:55 +0800

    Completed refactoring TopBar and LeftDrawer.

--- src/components/Templates.js
+++ src/components/Templates.js
@@ -1,83 +1,26 @@
 import React from 'react';
-import clsx from 'clsx';
-import { connect } from "react-redux";
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import { ThemeProvider } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Avatar from "@material-ui/core/Avatar";
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { mainListItems } from './listItems';
-import theme from "../themes";
-import {paragraphFiller} from "../fixtures";
-import {startLogout} from "../actions/auth";
+import { paragraphFiller } from "../fixtures";
 import templateStyles from "../styles/makeStyles/makeTemplateStyles";
 import TopBar from "./defaults/TopBar";
-import {templateDrawerChange} from "../actions/utility";
+import LeftDrawer from "./defaults/LeftDrawer";
 
 const useStyles = makeStyles(templateStyles);
 
-const Dashboard = (props) => {
+const Dashboard = () => {
     const classes = useStyles();
-    // const [open, setOpen] = React.useState(false);
-    // const handleDrawerOpen = () => setOpen(true);
-    const handleDrawerClose = () => props.dispatchDrawer();
-
 
     return (
         <div className={classes.root}>
             <CssBaseline />
             {/* Top Nav Bar */}
             <TopBar />
-            {/*<ThemeProvider theme={theme}>*/}
-            {/*    <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>*/}
-            {/*        <Toolbar className={classes.toolbar}>*/}
-            {/*            <IconButton*/}
-            {/*                edge="start" color="inherit" aria-label="open drawer" onClick={handleDrawerOpen}*/}
-            {/*                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}*/}
-            {/*            >*/}
-            {/*                <MenuIcon />*/}
-            {/*            </IconButton>*/}
-            {/*            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>*/}
-            {/*                TopScore*/}
-            {/*            </Typography>*/}
-            {/*            <div className={classes.root}>*/}
-            {/*                <Button className={classes.logOutButton}*/}
-            {/*                        onClick={props.dispatchLogOut}*/}
-            {/*                >*/}
-            {/*                    Log Out*/}
-            {/*                </Button>*/}
-            {/*                <Avatar className = {"header__avatar"}> {props.nameInitial} </Avatar>*/}
-            {/*            </div>*/}
-            {/*        </Toolbar>*/}
-            {/*    </AppBar>*/}
-            {/*</ThemeProvider>*/}
             {/* Left Side Drawer */}
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !props.drawerOpen && classes.drawerPaperClose),
-                }}
-                open={props.drawerOpen}
-            >
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>{mainListItems}</List>
-            </Drawer>
+            <LeftDrawer />
             {/* Main Content */}
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
@@ -97,12 +40,4 @@ const Dashboard = (props) => {
     );
 }
 
-const mapStateToProps = (state) => ({
-    drawerOpen: state.util.templateDrawerOpen
-})
-const mapDispatchToProps = (dispatch) => ({
-    dispatchLogOut: () => dispatch(startLogout()),
-    dispatchDrawer: () => dispatch(templateDrawerChange())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
